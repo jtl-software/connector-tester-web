d79b0ab94d447b0502717d7fb7f5fe23e700da55
Patrick Gugelsberger <pgugelsberger@gmail.com>
2023-06-07 14:07:47 +0200

    update LinkingsController

--- src/Controller/LinkingsController.php
+++ src/Controller/LinkingsController.php
@@ -3,17 +3,13 @@
 namespace Jtl\ConnectorTester\Controller;
 
 use GuzzleHttp\Client as HttpClient;
+use GuzzleHttp\Exception\GuzzleException;
 use Jtl\Connector\Client\ConnectorClient;
 use Jtl\Connector\Core\Model\Identities;
 use Jtl\Connector\Core\Model\Identity;
 
 class LinkingsController extends ConnectorClient
 {
-    /**
-     * @var string
-     */
-    private string $response;
-
     /**
      * @param string $token
      * @param string $endpointUrl
@@ -30,46 +26,31 @@ class LinkingsController extends ConnectorClient
      */
     public function clearLinkings(): string
     {
-        $this->setResponse($this->clear());
-        return \json_encode($this->getResponse() ? 'Linkings cleared' : 'Failed to clear linkings');
+        $response = $this->clear();
+        return $response ? 'Linkings cleared' : 'Failed to clear linkings';
     }
 
     /**
      * @param string $controller
      * @param string $payload
      * @return string
+     * @throws GuzzleException
      */
     public function clearLinkingsFromJson(string $controller, string $payload): string
     {
         $this->setFullResponse(true);
         $identities      = new Identities();
         $identitiesArray = [];
-        $payload         = \json_decode($payload, \JSON_OBJECT_AS_ARRAY);
+        $payload         = \json_decode($payload, true, 512, \JSON_OBJECT_AS_ARRAY);
 
+        /** @var array<int, array{0: numeric-string, 1: int}> $payload */
         foreach ($payload as $item) {
-            $identity                       = new Identity($item[0], $item[1]);
-            $identitiesArray[$controller][] = $identity;
+            $identitiesArray[] = new Identity($item[0], $item[1]);
         }
-
         $identities->setIdentities($identitiesArray);
-        return \json_encode($this->clearFromJson($identities), \JSON_PRETTY_PRINT);
-    }
 
-    /**
-     * @return string
-     */
-    public function getResponse(): string
-    {
-        return $this->response;
-    }
+        $response = \json_encode($this->clearFromJson($identities, $controller), \JSON_PRETTY_PRINT);
 
-    /**
-     * @param string $response
-     * @return LinkingsController
-     */
-    public function setResponse(string $response): LinkingsController
-    {
-        $this->response = $response;
-        return $this;
+        return $response ? $response : "Couldn't clear Linkings";
     }
 }
