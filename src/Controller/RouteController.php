83dcc48391c9170d08382190b82672072d0915e7
Patrick Gugelsberger <pgugelsberger@gmail.com>
2023-06-14 16:34:25 +0200

    update RouteController to catch https redirect error

--- src/Controller/RouteController.php
+++ src/Controller/RouteController.php
@@ -2,6 +2,7 @@
 
 namespace Jtl\ConnectorTester\Controller;
 
+use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
 use Nyholm\Psr7\Stream;
 use Psr\Http\Message\ResponseInterface;
@@ -9,6 +10,13 @@ use Psr\Http\Message\ServerRequestInterface;
 
 class RouteController
 {
+    private Client $client;
+
+    public function __construct()
+    {
+        $this->client = new Client(['allow_redirects' => false]);
+    }
+
     /**
      * @param ServerRequestInterface $request
      * @param ResponseInterface $response
@@ -17,10 +25,22 @@ class RouteController
     public function authenticate(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
     {
         $attributes     = $this->getAttributes($request);
-        $authController = new AuthController($attributes['connectorToken'], $attributes['connectorUrl']);
+        $authController = new AuthController($attributes['connectorToken'], $attributes['connectorUrl'], $this->client);
         $response->getBody()->write($authController->startAuth());
 
         return $response;
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     *
+     * @return array<string, string>
+     */
+    private function getAttributes(ServerRequestInterface $request): array
+    {
+        /** @var array<string, string> $attributes */
+        $attributes = $request->getParsedBody();
+        return $attributes;
     }
 
     /**
@@ -31,7 +51,7 @@ class RouteController
     public function disconnect(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
     {
         $attributes     = $this->getAttributes($request);
-        $authController = new AuthController($attributes['connectorToken'], $attributes['connectorUrl']);
+        $authController = new AuthController($attributes['connectorToken'], $attributes['connectorUrl'], $this->client);
         $authController->disconnect();
 
         return $response;
@@ -41,11 +61,16 @@ class RouteController
      * @param ServerRequestInterface $request
      * @param ResponseInterface $response
      * @return ResponseInterface
+     * @throws \JsonException
      */
     public function pull(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
     {
         $attributes       = $this->getAttributes($request);
-        $actionController = new ActionController($attributes['connectorToken'], $attributes['connectorUrl']);
+        $actionController = new ActionController(
+            $attributes['connectorToken'],
+            $attributes['connectorUrl'],
+            $this->client
+        );
         $response->getBody()->write($actionController->controllerPull($attributes['controller']));
 
         return $response;
@@ -55,11 +80,16 @@ class RouteController
      * @param ServerRequestInterface $request
      * @param ResponseInterface $response
      * @return ResponseInterface
+     * @throws \JsonException
      */
     public function push(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
     {
         $attributes       = $this->getAttributes($request);
-        $actionController = new ActionController($attributes['connectorToken'], $attributes['connectorUrl']);
+        $actionController = new ActionController(
+            $attributes['connectorToken'],
+            $attributes['connectorUrl'],
+            $this->client
+        );
         $response->getBody()->write(
             $actionController->controllerPush($attributes['controller'], $attributes['payload'])
         );
@@ -71,11 +101,16 @@ class RouteController
      * @param ServerRequestInterface $request
      * @param ResponseInterface $response
      * @return ResponseInterface
+     * @throws \JsonException
      */
     public function delete(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
     {
         $attributes       = $this->getAttributes($request);
-        $actionController = new ActionController($attributes['connectorToken'], $attributes['connectorUrl']);
+        $actionController = new ActionController(
+            $attributes['connectorToken'],
+            $attributes['connectorUrl'],
+            $this->client
+        );
         $response->getBody()->write(
             $actionController->controllerDelete($attributes['controller'], $attributes['payload'])
         );
@@ -88,11 +123,16 @@ class RouteController
      * @param ResponseInterface $response
      * @return ResponseInterface
      * @throws GuzzleException
+     * @throws \JsonException
      */
     public function stats(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
     {
         $attributes       = $this->getAttributes($request);
-        $actionController = new ActionController($attributes['connectorToken'], $attributes['connectorUrl']);
+        $actionController = new ActionController(
+            $attributes['connectorToken'],
+            $attributes['connectorUrl'],
+            $this->client
+        );
         $response->getBody()->write($actionController->controllerStats($attributes['controller']));
 
         return $response;
@@ -102,11 +142,16 @@ class RouteController
      * @param ServerRequestInterface $request
      * @param ResponseInterface $response
      * @return ResponseInterface
+     * @throws \JsonException
      */
     public function finish(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
     {
         $attributes       = $this->getAttributes($request);
-        $actionController = new ActionController($attributes['connectorToken'], $attributes['connectorUrl']);
+        $actionController = new ActionController(
+            $attributes['connectorToken'],
+            $attributes['connectorUrl'],
+            $this->client
+        );
         $response->getBody()->write($actionController->connectorFinish());
 
         return $response;
@@ -116,11 +161,16 @@ class RouteController
      * @param ServerRequestInterface $request
      * @param ResponseInterface $response
      * @return ResponseInterface
+     * @throws \JsonException
      */
     public function identify(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
     {
         $attributes       = $this->getAttributes($request);
-        $actionController = new ActionController($attributes['connectorToken'], $attributes['connectorUrl']);
+        $actionController = new ActionController(
+            $attributes['connectorToken'],
+            $attributes['connectorUrl'],
+            $this->client
+        );
         $response->getBody()->write($actionController->connectorIdentify());
 
         return $response;
@@ -134,7 +184,11 @@ class RouteController
     public function clear(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
     {
         $attributes       = $this->getAttributes($request);
-        $actionController = new LinkingsController($attributes['connectorToken'], $attributes['connectorUrl']);
+        $actionController = new LinkingsController(
+            $attributes['connectorToken'],
+            $attributes['connectorUrl'],
+            $this->client
+        );
         $response->getBody()->write($actionController->clearLinkings());
 
         return $response;
@@ -144,12 +198,16 @@ class RouteController
      * @param ServerRequestInterface $request
      * @param ResponseInterface $response
      * @return ResponseInterface
-     * @throws GuzzleException
+     * @throws GuzzleException|\JsonException
      */
     public function features(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
     {
         $attributes       = $this->getAttributes($request);
-        $actionController = new ActionController($attributes['connectorToken'], $attributes['connectorUrl']);
+        $actionController = new ActionController(
+            $attributes['connectorToken'],
+            $attributes['connectorUrl'],
+            $this->client
+        );
         $response->getBody()->write($actionController->coreFeatures());
 
         return $response;
@@ -159,12 +217,16 @@ class RouteController
      * @param ServerRequestInterface $request
      * @param ResponseInterface $response
      * @return ResponseInterface
-     * @throws GuzzleException
+     * @throws GuzzleException|\JsonException
      */
     public function init(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
     {
         $attributes       = $this->getAttributes($request);
-        $actionController = new ActionController($attributes['connectorToken'], $attributes['connectorUrl']);
+        $actionController = new ActionController(
+            $attributes['connectorToken'],
+            $attributes['connectorUrl'],
+            $this->client
+        );
         $response->getBody()->write($actionController->coreInit());
 
         return $response;
@@ -174,11 +236,16 @@ class RouteController
      * @param ServerRequestInterface $request
      * @param ResponseInterface $response
      * @return ResponseInterface
+     * @throws \JsonException
      */
     public function triggerAck(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
     {
         $attributes    = $this->getAttributes($request);
-        $devController = new DevOptionsController($attributes['connectorToken'], $attributes['connectorUrl']);
+        $devController = new DevOptionsController(
+            $attributes['connectorToken'],
+            $attributes['connectorUrl'],
+            $this->client
+        );
         $response->getBody()->write($devController->triggerAck($attributes['controller'], $attributes['results']));
 
         return $response;
@@ -188,11 +255,16 @@ class RouteController
      * @param ServerRequestInterface $request
      * @param ResponseInterface $response
      * @return ResponseInterface
+     * @throws \JsonException
      */
     public function getSkeleton(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
     {
         $attributes    = $this->getAttributes($request);
-        $devController = new DevOptionsController($attributes['connectorToken'], $attributes['connectorUrl']);
+        $devController = new DevOptionsController(
+            $attributes['connectorToken'],
+            $attributes['connectorUrl'],
+            $this->client
+        );
         $response->getBody()->write($devController->getSkeleton($attributes['controller']));
 
         return $response;
@@ -202,12 +274,16 @@ class RouteController
      * @param ServerRequestInterface $request
      * @param ResponseInterface $response
      * @return ResponseInterface
-     * @throws GuzzleException
+     * @throws GuzzleException|\JsonException
      */
     public function pushTest(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
     {
         $attributes    = $this->getAttributes($request);
-        $devController = new DevOptionsController($attributes['connectorToken'], $attributes['connectorUrl']);
+        $devController = new DevOptionsController(
+            $attributes['connectorToken'],
+            $attributes['connectorUrl'],
+            $this->client
+        );
         $response->getBody()->write($devController->pushTest());
 
         return $response;
@@ -221,7 +297,11 @@ class RouteController
     public function clearFromJson(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
     {
         $attributes         = $this->getAttributes($request);
-        $linkingsController = new LinkingsController($attributes['connectorToken'], $attributes['connectorUrl']);
+        $linkingsController = new LinkingsController(
+            $attributes['connectorToken'],
+            $attributes['connectorUrl'],
+            $this->client
+        );
         $response->getBody()->write(
             $linkingsController->clearLinkingsFromJson($attributes['controller'], $attributes['payload'])
         );
@@ -242,17 +322,5 @@ class RouteController
             throw new \RuntimeException('$file must not be false');
         }
         return $response->withBody(new Stream($file));
-    }
-
-    /**
-     * @param ServerRequestInterface $request
-     *
-     * @return array<string, string>
-     */
-    private function getAttributes(ServerRequestInterface $request): array
-    {
-        /** @var array<string, string> $attributes */
-        $attributes = $request->getParsedBody();
-        return $attributes;
     }
 }
